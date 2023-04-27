fd62580b244fe25f044668b6b8550840cf6372e4
Geraldine <gerahernan.al@gmail.com>
2023-04-27 13:46:27 -0700

    feat: casting

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -12,12 +12,12 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case ADD_TO_CART:
             return {
-                total: state.total + action.payload.price,
+                total: state.total + +action.payload.price,
                 cart: [...state.cart, action.payload]
             };
         case REMOVE_FROM_CART:
             return {
-                total: state.total - action.payload.price,
+                total: state.total - +action.payload.price,
                 cart: state.cart.filter((product) => product.id !== action.payload.id)
             };
 
