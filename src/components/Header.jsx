6bb9082a8ac45867948943f2e6810f437a35f5c8
Geraldine <gerahernan.al@gmail.com>
2023-04-28 10:46:41 -0700

    feat: changing navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,33 +3,55 @@ const Header = () => {
   return (
     <header>
       <nav
-        className="relative flex w-full items-center justify-end bg-white py-2 text-neutral-600 shadow-lg hover:text-blue-700 focus:text-blue-700 dark:bg-blue-600 dark:text-neutral-200 md:flex-wrap md:justify-end"
+        className="bg-white border-gray-200 dark:bg-blue-900"
         data-te-navbar-ref
       >
-        <div className="flex w-full flex-wrap items-center justify-end px-3">
-          <div className="flex items-center">
-            <button
-              className="border-0 bg-transparent px-2 text-xl leading-none transition-shadow duration-150 ease-in-out hover:text-blue-700 focus:text-neutral-700 dark:hover:text-fuchsia-200 dark:focus:text-white lg:hidden"
-              type="button"
-              data-te-collapse-init
-              data-te-target="#navbarSupportedContentY"
-              aria-controls="navbarSupportedContentY"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            ></button>
-          </div>
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link to={"https://github.com/chicacode/kaftka-app"} className="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8 mr-3"
+              alt="kaftka Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              kaftka
+            </span>
+          </Link>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-200 dark:text-blue-400 "
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
 
           <div
-            id="navbarSupportedContentY"
+            className="hidden w-full md:block md:w-auto"
+            id="navbar-default"
             data-te-collapse-item
           >
             <ul
-              className="mr-auto flex flex-col lg:flex-row"
+              className="mr-auto flex flex-col lg:flex-row items-center"
               data-te-navbar-nav-ref
             >
               <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
                 <Link
-                  className="block transition duration-150 ease-in-out text-blue-100 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-fuchsia-200 dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  className="block transition duration-150 ease-in-out text-blue-100 hover:text-blue-700 focus:text-blue-700 disabled:text-black/30 dark:hover:text-fuchsia-200 "
                   to={"/"}
                   data-te-nav-link-ref
                   data-te-ripple-init
@@ -38,31 +60,30 @@ const Header = () => {
                   Products
                 </Link>
               </li>
-              
-                <li className="mb-2 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                  <Link
-                    className="block transition duration-150 ease-in-out text-blue-100 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-fuchsia-200 dark:focus:text-white lg:p-2 [&.active]:text-black/90"
-                    to={"/products/new"}
-                    data-te-nav-link-ref
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
-                  >
-                    Add new product &#43;
-                  </Link>
-                </li>
-                <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                  <Link
-                    className="block transition duration-150 ease-in-out text-blue-100 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-fuchsia-200 dark:focus:text-white lg:p-2 [&.active]:text-black/90"
-                    to={"/cart"}
-                    data-te-nav-link-ref
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
-                  >
-                    Cart
-                    <i className="fa-solid fa-cart-shopping"></i>
-                  </Link>
-                </li>
-             
+
+              <li className="mb-2 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link
+                  className="block transition duration-150 ease-in-out text-blue-100 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-fuchsia-200 dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  to={"/products/new"}
+                  data-te-nav-link-ref
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Add new product &#43;
+                </Link>
+              </li>
+              <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link
+                  className="block transition duration-150 ease-in-out text-blue-100 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-fuchsia-200 dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  to={"/cart"}
+                  data-te-nav-link-ref
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Cart
+                  <i className="fa-solid fa-cart-shopping"></i>
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
